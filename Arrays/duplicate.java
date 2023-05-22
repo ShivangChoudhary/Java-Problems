779eceee7dfb5db9100964bb895896c9f6f45ebb
Shivang Choudhary <shivang.ch007@gmail.com>
2023-05-22 14:48:27 +0530

    Create arrayOfNames.java

---
+++ Arrays/duplicate.java
@@ -0,0 +1,15 @@
+package array;
+
+public class duplicate {
+    public static void main(String[] args) {
+        int arr[]={3,5,5,4,2,2,4};
+        for (int i=0;i< arr.length;i++){
+            for (int j =i+1;j< arr.length;j++){
+                if (arr[i]==arr[j]){
+                    System.out.println(arr[i]);
+                }
+                }
+            }
+        }
+    }
+
