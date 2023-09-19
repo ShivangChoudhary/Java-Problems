56b39c17cb1e1cf6ffe9252d1f5658ea973a0e09
Shivang Choudhary <133525971+ShivangChoudhary@users.noreply.github.com>
2023-09-19 20:41:52 +0530

    Add files via upload

---
+++ Arrays/prac.java
@@ -0,0 +1,15 @@
+package array;
+import java.util.Scanner;
+
+public class prac {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a);
+            System.out.println(b);
+
+        }
+    }
+
