45acf4b2a86ace8ad490c08577bea704e8839e3f
Shivang Choudhary <shivang.ch007@gmail.com>
2023-11-01 19:33:55 +0530

    Create algo.java

---
+++ selectionSort/algo.java
@@ -0,0 +1,43 @@
+package selectionSort;
+
+import java.util.Arrays;
+
+public class algo {
+    public static void main(String[] args) {
+        int arr[] = {3, 1, 5, 4, 2};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    public static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int max = maxele(arr, 0, last);
+            swapp(arr, max,last);
+        }
+
+
+
+
+    }
+
+    private static void swapp(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+
+    private static int maxele(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+
+            }
+        }
+        return max;
+    }
+}
