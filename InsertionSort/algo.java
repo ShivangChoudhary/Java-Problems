a715b5f74e5e251b1546aabbde194276558d409e
Shivang Choudhary <shivang.ch007@gmail.com>
2023-11-02 20:41:06 +0530

    Create algo.java

---
+++ InsertionSort/algo.java
@@ -0,0 +1,32 @@
+package InsertionSort;
+
+import java.util.Arrays;
+
+public class algo {
+    public static void main(String[] args) {
+    int arr[]={2,6,9,1,7,8};
+    insertion(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void insertion (int[]arr){
+        for (int i=0;i< arr.length-1;i++){
+            for (int j=i+1;j>0;j--){
+                if (arr[j-1]>arr[j]){
+                    swap(arr,j-1,j);
+                }else {
+                    break;
+                }
+
+            }
+        }
+
+    }
+
+   static void swap(int []arr,int first, int second) {
+        int temp = arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+
+}
