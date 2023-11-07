d078620f6b0e26a9c76d82bff6a9de59e545cb95
Shivang Choudhary <shivang.ch007@gmail.com>
2023-11-07 17:32:53 +0530

    pattern

---
+++ patterns/patt3.java
@@ -0,0 +1,13 @@
+package patterns;
+
+public class patt3 {
+    public static void main(String[] args) {
+        int n=5;
+        for (int row=1;row<=n;row++){
+            for (int col=5;col>=row;col--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
