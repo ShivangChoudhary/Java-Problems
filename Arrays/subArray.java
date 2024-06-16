f9443eb3974d8967322c4574e4cff73a71a343d6
Shivang Choudhary <133525971+ShivangChoudhary@users.noreply.github.com>
2024-06-16 23:39:28 +0530

    Add files via upload

---
+++ Arrays/subArray.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class subArray {
+    public static void printSubArray(int arr[]){
+        for (int i=0;i<arr.length;i++){
+            int start = i;
+            for(int j=i;j<arr.length;j++){
+                int end =j;
+                for (int k=start;k<=end;k++){
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8};
+        printSubArray(arr);
+    }
+}
