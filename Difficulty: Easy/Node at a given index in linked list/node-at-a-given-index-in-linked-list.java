25ea8c280a484301bbad449d5aaf62762b067a9a
Shivang Choudhary <133525971+ShivangChoudhary@users.noreply.github.com>
2024-07-28 00:15:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Node at a given index in linked list/node-at-a-given-index-in-linked-list.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class LinkedList {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            int k = Integer.parseInt(br.readLine());
+            Node head = new Node(arr.get(0));
+            Node tail = head;
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            int res = ob.GetNth(head, k);
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public int GetNth(Node head, int index) {
+        // Code here
+        int len =0;
+        Node temp = head;
+        Node temp2=head;
+        while(temp!=null){
+            len++;
+            temp=temp.next;
+        }
+        if(index>len){
+            return -1;
+        }
+        for(int i=0;i<index-1;i++){
+            temp2=temp2.next;
+            
+        }
+        return temp2.data;
+    }
+}
